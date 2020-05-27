ae4dae3eecc81eee4124c2b68899fadc81209784
ijbd <ijbd@umich.edu>
2020-05-27 14:18:59 -0600

    Testing wind shear.

--- powGen.sh
+++ powGen.sh
@@ -3,10 +3,11 @@
 # This is the shell driver for the powGen program. It simulates solar and wind power 
 # by repeatedly running the Power_Generation_.8.py longitude by longitude. 
 
-python checkForFinish.py
+
 finished=$?
 year=2018
 logFile=2018_test_default_shear
+python checkForFinish.py $logFile
 
 while [ $finished -eq 0 ]
 do
