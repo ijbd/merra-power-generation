ee8cfd177e27900b31161201b39aa4dcb89de5ed
ijbd <ijbd@umich.edu>
2020-06-04 15:40:06 -0600

    Lat/Lon test

--- powGen_impl.py
+++ powGen_impl.py
@@ -20,20 +20,16 @@ log_file = sys.argv[3]
 print('Log file and year: ' + log_file + ' ' + str(year),flush=True)
 
 
-def get_lat_lon_new(processed_merra_file):
+def get_lat_lon(processed_merra_file):
+    
+    processed_merra_file = "scratch/mtcraig_root/mtcraig1/shared_data/merraData/resource/wecc/raw/MERRA2_400.tavg1_2d_rad_Nx.20181231.nc4.nc4"
+
     data = Dataset(processed_merra_file)
-    lats = np.array(data.variables['lat'])
-    lons = np.array(data.variables['lon'])
+    lats = np.array(data.variables['lat'][:3])
+    lons = np.array(data.variables['lon'][:3])
     data.close()
     num_lats = lats.size
     num_lons = lons.size
-    return lats, lons, num_lats, num_lons
-
-def get_lat_lon(ignore):
-    lats = np.arange(3)
-    lons = np.arange(3)
-    num_lats = 3
-    num_lons = 3
     return lats, lons, num_lats, num_lons
 
 
