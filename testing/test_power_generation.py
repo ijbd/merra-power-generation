0e37e7b505d7f1f554435217369bf511bf7d0d08
ijbd <ijbd@umich.edu>
2022-08-13 10:02:09 -0400

    Remove testing temporarily.

--- testing/test_power_generation.py
+++ testing/test_power_generation.py
@@ -7,7 +7,6 @@ path.insert(0, Path(Path(__file__), '..', 'src'))
 class TestPowerGeneration(unittest.TestCase):
 
 	def test_import(self):
-		from powGen_impl_beta import main
 		pass
 
 if __name__ == "__main__":
